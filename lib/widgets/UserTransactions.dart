a892be62e6f46d4ca01a277b271519414ce29aad
Rahul Ajith <RahulAjith007>
2020-10-11 19:23:23 +0530

    Initial Commit

---
+++ lib/widgets/UserTransactions.dart
@@ -0,0 +1,25 @@
+import 'package:flutter/material.dart';
+import './NewTransaction.dart';
+import './TransactionList.dart';
+import '../models/transaction.dart';
+
+class UserTransaction extends StatefulWidget {
+  @override
+  _UserTransactionState createState() => _UserTransactionState();
+}
+
+class _UserTransactionState extends State<UserTransaction> {
+  final List<Transaction> _userTransactions = [
+    Transaction(
+        id: 't1', title: 'New Shoes', amount: 69.99, date: DateTime.now()),
+    Transaction(
+        id: 't1', title: 'New Keys', amount: 69.99, date: DateTime.now()),
+  ];
+
+  @override
+  Widget build(BuildContext context) {
+    return Column(
+      children: <Widget>[NewTransaction(), TransactionList()],
+    );
+  }
+}
